e0e7a1b4a53b642677e317e14bbee35378c66f0d
Joe <joe@joebailey.xyz>
2021-03-10 22:36:37 +0000

    Initial commit

---
+++ backup.php
@@ -0,0 +1,46 @@
+<?php
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: GET, OPTIONS");
+    header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+    header("Content-Type: application/json; charset=UTF-8");
+    header("X-Frame-Options: DENY");
+    header("Strict-Transport-Security: max-age=15552000; preload");
+    header("Content-Security-Policy: default-src 'self'");
+    header("Referrer-Policy: no-referrer");
+    header("X-Content-Type-Options: nosniff");
+    require_once __DIR__ . '/vendor/autoload.php';
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+    $dotenv->load();
+    $dotenv->required(['DATABASE_HOST', 'DATABASE_NAME', 'DATABASE_USER'])->notEmpty();
+    function generateRandomString($length = 10) {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return $randomString;
+    }
+    //Enter your database information here and the name of the backup file
+    $mysqlDatabaseName = $_ENV['DATABASE_NAME'];
+    $mysqlUserName = $_ENV['DATABASE_USER'];
+    $mysqlPassword = $_ENV['DATABASE_PASS'];
+    $mysqlHostName = $_ENV['DATABASE_HOST'];
+    $mysqlExportPath = generateRandomString() . 'Galexia_Backup.sql';
+
+    //Please do not change the following points
+    //Export of the database and output of the status
+    $command='mysqldump --opt -h' .$mysqlHostName .' -u' .$mysqlUserName .' -p' .$mysqlPassword .' ' .$mysqlDatabaseName .' > ' .$mysqlExportPath;
+    exec($command, $output=array(), $worked);
+    switch($worked){
+        case 0:
+            return http_response_code( 200 );
+            break;
+        case 1:
+            return http_response_code( 500 );
+            break;
+        case 2:
+            return http_response_code( 500 );
+            break;
+    }
+?>
---
+++ index.php
@@ -0,0 +1,163 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Access-Control-Allow-Headers: Authorization, Origin, X-Requested-With, Content-Type, Accept");
+header("Content-Type: application/json; charset=UTF-8");
+header("X-Frame-Options: DENY");
+header("Strict-Transport-Security: max-age=15552000; preload");
+header("Content-Security-Policy: default-src 'self'");
+header("Referrer-Policy: no-referrer");
+header("X-Content-Type-Options: nosniff");
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+$dotenv->required(['DATABASE_HOST', 'DATABASE_NAME', 'DATABASE_USER', 'DATABASE_PASS'])->notEmpty();
+
+// Don't do anything for prefetch requests.
+if ( $_SERVER['REQUEST_METHOD'] === 'OPTIONS' ) {
+    return http_response_code( 200 );
+};
+
+$db_host = $_ENV['DATABASE_HOST'];
+$db_name = $_ENV['DATABASE_NAME'];
+$db_user = $_ENV['DATABASE_USER'];
+$db_pass = $_ENV['DATABASE_PASS'];
+
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
+// Connect to the database
+$conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
+
+$conn->set_charset("utf8mb4");
+
+// Import classes from the Psr library (standardised HTTP requests and responses)
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+use Slim\Factory\AppFactory;
+
+// Create our app.
+$app = AppFactory::create();
+
+// Add routing functionality to Slim. This is not included by default and
+// must be turned on.
+$app->addRoutingMiddleware();
+
+// Add error handling functionality. The three 'true's indicate:
+// - first argument: display full error details
+// - second argument: call Slim error handler
+// - third argument: log error details
+
+$app->addErrorMiddleware(true, true, true);
+ 
+// For the routes to work correctly, you must set your base path.
+// This is the relative path of your webspace on the server, including the
+// folder you're using but NOT public_html. Here we are assuming the Slim app
+// is saved in the 'slimapp' folder within 'public_html' 
+$app->setBasePath('');
+
+// Create our PHP renderer object
+$view = new \Slim\Views\PhpRenderer('views');
+
+$app->get('/', function (Request $req, Response $res, array $args) use($conn) {
+    $stmt = $conn->prepare("SELECT * FROM clients");
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $clients = array();
+    while($row = $result->fetch_assoc()) {
+        if($row) {
+            $clients[] = $row;
+        }
+        else {
+            return $res->withJson(null);
+        }
+    };
+    $stmt->close();
+
+    $stmt = $conn->prepare("SELECT * FROM contacts");
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $contacts = array();
+    while($row = $result->fetch_assoc()) {
+        if($row) {
+            $contacts[] = $row;
+        }
+        else {
+            return $res->withJson(null);
+        }
+    };
+    $stmt->close();
+
+    $stmt = $conn->prepare("SELECT * FROM domains");
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $domains = array();
+    while($row = $result->fetch_assoc()) {
+        if($row) {
+            $domains[] = $row;
+        }
+        else {
+            return $res->withJson(null);
+        }
+    };
+    $stmt->close();
+
+    $stmt = $conn->prepare("SELECT * FROM projects");
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $projects = array();
+    while($row = $result->fetch_assoc()) {
+        if($row) {
+            $projects[] = $row;
+        }
+        else {
+            return $res->withJson(null);
+        }
+    };
+    $stmt->close();
+
+    $response = array();
+    $response[0] = $clients;
+    $response[1] = $contacts;
+    $response[2] = $domains;
+    $response[3] = $projects;
+
+    return $res->withJson($response);
+});
+
+$app->post('/projects/lists', function (Request $req, Response $res) use($conn) {
+    $post = $req->getParsedBody();
+    $stmt = $conn->prepare("UPDATE projects SET lists = ? WHERE id = ?");
+    $stmt->bind_param("si", $post["lists"], $post["id"]);
+    $stmt->execute();
+    $stmt->close();
+    return $res->withJson('Updated successfuly.');
+});
+
+$app->put('/projects', function (Request $req, Response $res) use($conn) {
+    $post = $req->getParsedBody();
+    $stmt = $conn->prepare("INSERT into projects (client_id, name, status, hosting, github_url) VALUES (?, ?, ?, ?, ?)");
+    $stmt->bind_param("issss", $post["client_id"], $post["name"], $post["status"], $post["hosting"], $post["github_url"]);
+    $stmt->execute();
+    $stmt->close();
+
+    $stmt = $conn->prepare("SELECT * FROM projects");
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $projects = array();
+    while($row = $result->fetch_assoc()) {
+        if($row) {
+            $projects[] = $row;
+        }
+        else {
+            return $res->withJson(null);
+        }
+    };
+    $stmt->close();
+
+    return $res->withJson($projects);
+});
+
+$app->run();
