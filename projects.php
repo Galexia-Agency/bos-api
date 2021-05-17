d5be33572a0911e78df629bede0114dc428e13cd
Joe Bailey <joe@joebailey.xyz>
2021-05-17 12:39:09 +0100

    Updated order of projects and response

--- projects.php
+++ projects.php
@@ -4,24 +4,6 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
-$app->post('/projects', function (Request $req, Response $res) use($conn) {
-    $post = $req->getParsedBody();
-    $stmt = $conn->prepare("UPDATE projects SET name = ?, status = ?, hosting = ?, github_url = ?, project_url = ?, project_login_url = ?, updated_at = ? WHERE id = ?");
-    $stmt->bind_param("sssssssi", $post["name"], $post["status"], $post["hosting"], $post["github_url"], $post['project_url'], $post['project_login_url'], date("Y-m-d H:i:s"), $post["id"]);
-    $stmt->execute();
-    $stmt->close();
-    return $res->withJson('Updated successfuly.');
-});
-
-$app->post('/projects/lists', function (Request $req, Response $res) use($conn) {
-    $post = $req->getParsedBody();
-    $stmt = $conn->prepare("UPDATE projects SET lists = ?, updated_at = ? WHERE id = ?");
-    $stmt->bind_param("ssi", $post["lists"], date("Y-m-d H:i:s"), $post["id"]);
-    $stmt->execute();
-    $stmt->close();
-    return $res->withJson('Updated successfuly.');
-});
-
 $app->put('/projects', function (Request $req, Response $res) use($conn) {
     $post = $req->getParsedBody();
     $stmt = $conn->prepare("INSERT into projects (client_id, name, status, project_url, project_login_url, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
@@ -44,6 +26,58 @@ $app->put('/projects', function (Request $req, Response $res) use($conn) {
     $stmt->close();
 
     return $res->withJson($projects);
+});
+
+$app->post('/projects', function (Request $req, Response $res) use($conn) {
+    $post = $req->getParsedBody();
+    $stmt = $conn->prepare("UPDATE projects SET name = ?, status = ?, hosting = ?, github_url = ?, project_url = ?, project_login_url = ?, updated_at = ? WHERE id = ?");
+    $stmt->bind_param("sssssssi", $post["name"], $post["status"], $post["hosting"], $post["github_url"], $post['project_url'], $post['project_login_url'], date("Y-m-d H:i:s"), $post["id"]);
+    $stmt->execute();
+    $stmt->close();
+    
+    $stmt = $conn->prepare("SELECT * FROM projects WHERE id = ?");
+    $stmt->bind_param("i", $post["id"]);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    $response = array();
+
+    while($row = $result->fetch_assoc()) {
+        if($row) {
+            $response[] = $row;
+        }
+        else {
+            return $res->withJson(null);
+        }
+    };
+
+    return $res->withJson($response);
+});
+
+$app->post('/projects/lists', function (Request $req, Response $res) use($conn) {
+    $post = $req->getParsedBody();
+    $stmt = $conn->prepare("UPDATE projects SET lists = ?, updated_at = ? WHERE id = ?");
+    $stmt->bind_param("ssi", $post["lists"], date("Y-m-d H:i:s"), $post["id"]);
+    $stmt->execute();
+    
+    $stmt->close();
+    $stmt = $conn->prepare("SELECT * FROM projects WHERE id = ?");
+    $stmt->bind_param("i", $post["id"]);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    $response = array();
+
+    while($row = $result->fetch_assoc()) {
+        if($row) {
+            $response[] = $row;
+        }
+        else {
+            return $res->withJson(null);
+        }
+    };
+
+    return $res->withJson($response);
 });
 
 ?>
