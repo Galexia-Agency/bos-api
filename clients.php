c0fec3ffa01cf83cb984b78c84e7e7908239fdc4
Joe <joe@joebailey.xyz>
2021-09-29 22:26:37 +0100

    Added about to clients

--- clients.php
+++ clients.php
@@ -6,8 +6,8 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 
 $app->put('/clients', function (Request $req, Response $res) use($conn) {
     $post = $req->getParsedBody();
-    $stmt = $conn->prepare("INSERT into clients (business_name, business_shortname, address, source, created_at, updated_at, pandle_id, billing_email) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
-    $stmt->bind_param("ssssssss", $post["business_name"], $post["business_shortname"], $post["address"], $post["source"], $dateTime, $dateTime, $post["pandle_id"], $post["billing_email"],);
+    $stmt = $conn->prepare("INSERT into clients (business_name, business_shortname, about, address, source, created_at, updated_at, pandle_id, billing_email) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
+    $stmt->bind_param("sssssssss", $post["business_name"], $post["business_shortname"], $post['about'], $post["address"], $post["source"], $dateTime, $dateTime, $post["pandle_id"], $post["billing_email"],);
     $stmt->execute();
     $stmt->close();
 
@@ -30,8 +30,8 @@ $app->put('/clients', function (Request $req, Response $res) use($conn) {
 
 $app->post('/clients', function (Request $req, Response $res) use($conn) {
     $post = $req->getParsedBody();
-    $stmt = $conn->prepare("UPDATE clients SET business_name = ?, business_shortname = ?, address = ?, source = ?, updated_at = ?, pandle_id = ?, billing_email = ? WHERE id = ?");
-    $stmt->bind_param("sssssssi", $post["business_name"], $post["business_shortname"], $post["address"], $post["source"], $dateTime, $post["pandle_id"], $post["billing_email"], $post["id"]);
+    $stmt = $conn->prepare("UPDATE clients SET business_name = ?, business_shortname = ?, about = ?, address = ?, source = ?, updated_at = ?, pandle_id = ?, billing_email = ? WHERE id = ?");
+    $stmt->bind_param("ssssssssi", $post["business_name"], $post["business_shortname"], $post['about'], $post["address"], $post["source"], $dateTime, $post["pandle_id"], $post["billing_email"], $post["id"]);
     $stmt->execute();
     $stmt->close();
 
