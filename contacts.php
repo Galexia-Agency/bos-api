1f0a8156151b0365da2b9bcb25491839500cc4d8
Joe Bailey <joe@joebailey.xyz>
2022-11-11 11:23:40 +0000

    Add 429 response code for contacts and comment out unused endpoints

--- contacts.php
+++ contacts.php
@@ -15,28 +15,47 @@ $scopes       = ['https://www.googleapis.com/auth/userinfo.profile', 'https://ww
 $googleOAuth2Handler = new GoogleOAuth2Handler($clientId, $clientSecret, $scopes, $refreshToken);
 $people = new GooglePeople($googleOAuth2Handler);
 
-$app->get('/contacts', function (Request $req, Response $res, array $args) use($conn) {
-    $stmt = $conn->prepare("SELECT * FROM contacts");
+function selectContactById ($conn, $id) {
+    $stmt = $conn->prepare("SELECT * FROM contacts WHERE id = ?");
+    $stmt->bind_param("i", $id);
     $stmt->execute();
     $result = $stmt->get_result();
-    $contacts = array();
-    while($row = $result->fetch_assoc()) {
-        if($row) {
-            $contacts[] = $row;
-        }
-        else {
-            return $res->withJson(null);
+
+    $response = array();
+
+    while ($row = $result->fetch_assoc()) {
+        if ($row) {
+            $response[] = $row;
         }
     };
-    $stmt->close();
+    if (empty($response)) {
+        return null;
+    }
+    return $response;
+}
 
-    return $res->withJson($contacts);
-});
+// $app->get('/contacts', function (Request $req, Response $res, array $args) use($conn) {
+//     $stmt = $conn->prepare("SELECT * FROM contacts");
+//     $stmt->execute();
+//     $result = $stmt->get_result();
+//     $contacts = array();
+//     while($row = $result->fetch_assoc()) {
+//         if($row) {
+//             $contacts[] = $row;
+//         }
+//         else {
+//             return $res->withJson(null);
+//         }
+//     };
+//     $stmt->close();
 
-$app->get('/google-contacts', function (Request $req, Response $res, array $args) use($conn) {
-    global $people;
-    return $res->withJson($people->all());
-});
+//     return $res->withJson($contacts);
+// });
+
+// $app->get('/google-contacts', function (Request $req, Response $res, array $args) use($conn) {
+//     global $people;
+//     return $res->withJson($people->all());
+// });
 
 $app->put('/contacts', function (Request $req, Response $res) use($conn) {
     global $people;
@@ -73,8 +92,21 @@ $app->post('/contacts', function (Request $req, Response $res) use($conn) {
     global $people;
     $post = $req->getParsedBody();
 
+    // Fetch the project by id from the database
+    $response = selectContactById($conn, $post["id"]);
+
+    // If we're forcing the change then skip the check for updated content
+    if (!$post["force"] && $post["updated_at"]) {
+        if ($response) {
+            if ($post["updated_at"] < $response[0]['updated_at']) {
+                return $res->withStatus(429)->withJson($response);
+            }
+        } else {
+            return $res->withJson(null);
+        }
+    }
+
     try {
-        $people->get($post["google_contact_id"]);
         $contact = $people->get($post["google_contact_id"]);
     } catch (exception $e) {
         $contact = new Contact($people);
@@ -87,23 +119,5 @@ $app->post('/contacts', function (Request $req, Response $res) use($conn) {
     $stmt->execute();
     $stmt->close();
 
-    $stmt = $conn->prepare("SELECT * FROM contacts WHERE id = ?");
-    $stmt->bind_param("i", $post["id"]);
-    $stmt->execute();
-    $result = $stmt->get_result();
-
-    $response = array();
-
-    while($row = $result->fetch_assoc()) {
-        if($row) {
-            $response[] = $row;
-        }
-        else {
-            return $res->withJson(null);
-        }
-    };
-
-    return $res->withJson($response);
+    return $res->withJson(selectContactById($conn, $post["id"]));
 });
-
-?>
